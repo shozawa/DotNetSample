943237ccd9a195234783606e82d2bd28cb0ce077
tomohiro shozawa <tomohiro.shozawa@gmail.com>
2021-01-24 18:01:42 +0900

    add unit test

---
+++ ConsoleApp/UnitTest/CurrencyTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using ConsoleApp;
+
+namespace UnitTest
+{
+    public class CurrencyTest
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            var a = new Currency(1);
+            var b = new Currency(2);
+            Assert.Equal(3, a.Add(b).Value);
+        }
+    }
+}
