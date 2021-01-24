7f5dbcb431aae752caa18321aeadd5118619239a
tomohiro shozawa <tomohiro.shozawa@gmail.com>
2021-01-24 17:43:10 +0900

    add Currency

---
+++ ConsoleApp/ConsoleApp/Currency.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp
+{
+    public class Currency
+    {
+        public decimal Value { get; private set; }
+
+        public Currency(decimal value)
+        {
+            Value = value;
+        }
+
+        public Currency Add(Currency currency)
+        {
+            return new Currency(Value + currency.Value);
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Currency(42);
+            var b = new Currency(42);
+            Console.WriteLine(a.Add(b).Value);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
